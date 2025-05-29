5d181d605b3d0a6eff14743b46ae2ad416ed5f62
5ceansdeep <su5653@naver.com>
2025-05-29 11:01:47 +0900

    ~#6.4

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-import { useState, useEffect, use } from "react";
+import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value); // 주석 좀.. 까먹음음
-
-  console.log("i run all the time");
-
+function Hello() {
+  // function byFn() {
+  //   console.log("bye :(");
+  // }
+  // function hiFn() {
+  //   console.log("created :)");
+  //   return byFn;
+  // }
   useEffect(() => {
-    console.log("CALL THE API...");
+    console.log("hi");
+    return () => console.log("bye");
   }, []);
 
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("SEARCH FOR", keyword);
-    } 
-  }, [keyword])
-  
+  // useEffect(function () {
+  //   console.log("hi");
+  //   return function () {
+  //     console.log("bye :(");
+  //   }
+  // }, []);
+
+  return <h1>Hello!</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search Here..."
-      />
-      <h1>{counter}!</h1>
-      <button onClick={onClick}>Click me!</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
